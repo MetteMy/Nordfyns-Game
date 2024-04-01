225ed30e526ed87b472d03f475f969b8d3e41ca7
MetteMy <mettemy@gabelgaard.org>
2024-04-01 11:54:38 +0200

    shooting

--- Nordfyns-Game/Assets/Scripts/Shooting.cs
+++ Nordfyns-Game/Assets/Scripts/Shooting.cs
@@ -5,21 +5,23 @@ using UnityEngine;
 public class Shooting : MonoBehaviour
 {
     public GameObject prefab;
+    public Transform bulletTransform;
+    private Camera mainCam;
+    private Vector2 mousePos;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        mainCam = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
     }
 
     // Update is called once per frame
     void Update()
     {
-         
+        mousePos = mainCam.ScreenToWorldPoint(Input.mousePosition);
         if (Input.GetKeyDown(KeyCode.Space)){
-       
-            Instantiate(prefab, transform.position, Quaternion.identity);
-        
+           
+            GameObject bullet = Instantiate(prefab, bulletTransform.position, Quaternion.identity);
         }}
-    
-    
+
 }
