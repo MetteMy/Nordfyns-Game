5f3a7be8116b09cc3c4f581873a13b1612c3219f
MetteMy <mettemy@gabelgaard.org>
2024-04-12 10:36:22 +0200

    interaction

---
+++ Nordfyns-Game/Assets/Scripts/Interaction.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interaction : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+
+    bool canInteract = false; 
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (canInteract == true &&  Input.GetKeyDown(KeyCode.E)){
+            Debug.Log("has been interacterd with "); 
+            canInteract = false; 
+
+           
+        }
+
+
+
+
+    }
+
+    void OnTriggerEnter2D(Collider2D other) { 
+
+        if (other.CompareTag("interactable" )){
+
+            canInteract = true; 
+            
+
+        }
+        Debug.Log("ENTERED");
+    }
+
+
+}
