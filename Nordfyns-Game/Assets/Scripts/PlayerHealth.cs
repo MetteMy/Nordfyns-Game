fa0078a3b8f836c60e4ce873e1ac5548131dcfaa
GustavNFHS <164423813+GustavNFHS@users.noreply.github.com>
2024-04-19 09:16:42 +0200

    health script

---
+++ Nordfyns-Game/Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+
+    public float health;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        health = 30.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (health == 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+
+        Debug.Log("Noget har ramt trigger");
+        if (other.CompareTag("enemybullet"))
+        {
+            health -= 1.0f;
+            Debug.Log("liv reduceret");
+        }
+    }
+
+}
