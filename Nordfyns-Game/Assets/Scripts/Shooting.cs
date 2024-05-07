63d8862a21bd86c2f02fdff84b874fe32c164a0b
MetteMy <mettemy@gabelgaard.org>
2024-05-07 14:59:30 +0200

    ewdsfertreaserf

--- Nordfyns-Game/Assets/Scripts/Shooting.cs
+++ Nordfyns-Game/Assets/Scripts/Shooting.cs
@@ -8,29 +8,31 @@ public class Shooting : MonoBehaviour
     public Transform bulletTransform;
     private Camera mainCam;
     private Vector3 mousePos;
-       
+
     // Start is called before the first frame update
     void Start()
     {
         mainCam = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
         mousePos = mainCam.ScreenToWorldPoint(Input.mousePosition);
-        
+
         Vector3 rotation = mousePos - transform.position;
 
-        float rotz = Mathf.Atan2(rotation.y, rotation.x) * Mathf.Rad2Deg;
+        float rot = Mathf.Atan2(rotation.y, rotation.x) * Mathf.Rad2Deg;
 
-        transform.rotation = Quaternion.Euler(0,0,rotz);
+        transform.rotation = Quaternion.Euler(0, 0, rot);
 
-        if (Input.GetKeyDown(KeyCode.Space)){
-           
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+
             GameObject bullet = Instantiate(prefab, bulletTransform.position, Quaternion.identity);
-            
-       }}
+
+        }
+    }
 
 }
